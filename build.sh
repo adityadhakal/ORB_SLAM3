a407c04c24031a0015947ad01ab0dffebb3e0c14
Chameleon Cloud User <cc@slam1.novalocal>
2021-05-07 10:15:26 +0000

    Build

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ echo "Configuring and building Thirdparty/DBoW2 ..."
 cd Thirdparty/DBoW2
 mkdir build
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release -DCUDA_TOOLKIT_ROOT_DIR=/usr/local/cuda-10.0
+cmake .. -DCMAKE_BUILD_TYPE=Release -DCUDA_INCLUDE_DIRS=/usr/local/cuda/include -DCUDA_CUDART_LIBRARY=/usr/local/cuda/lib64
 make -j
 
 cd ../../g2o
@@ -12,7 +12,7 @@ echo "Configuring and building Thirdparty/g2o ..."
 
 mkdir build
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release -DCUDA_TOOLKIT_ROOT_DIR=/usr/local/cuda-10.0
+cmake .. -DCMAKE_BUILD_TYPE=Release -DCUDA_INCLUDE_DIRS=/usr/local/cuda/include -DCUDA_CUDART_LIBRARY=/usr/local/cuda/lib64
 make -j
 
 cd ../../../
@@ -27,5 +27,5 @@ echo "Configuring and building ORB_SLAM3 ..."
 
 mkdir build
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release -DCUDA_TOOLKIT_ROOT_DIR=/usr/local/cuda-10.0
+cmake .. -DCMAKE_BUILD_TYPE=Release -DCUDA_INCLUDE_DIRS=/usr/local/cuda/include -DCUDA_CUDART_LIBRARY=/usr/local/cuda/lib64
 make -j
