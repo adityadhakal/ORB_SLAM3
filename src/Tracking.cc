b788027da331a923b495e0c75aff081b63b6a64b
Aditya <adityadhakal@github.com>
2022-03-15 12:26:40 +0100

    60 frames lost.

--- src/Tracking.cc
+++ src/Tracking.cc
@@ -1657,7 +1657,7 @@ void Tracking::ComputeVelocitiesAccBias(const vector<Frame*> &vpFs, float &bax, 
 void Tracking::Track()
 {
     static int framecounter = 0;
-    int imugap = 90;
+    int imugap = 60;
     cout<<"Frame COUNT: "<<framecounter<<" Timestamp: "<<mCurrentFrame.mTimeStamp<<endl;
 
     if (bStepByStep)
