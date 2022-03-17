f7c688b286a5872d8f0c912b3546f3d52c7e7df3
Aditya <adityadhakal@github.com>
2022-03-17 17:25:07 +0100

    stereo euroc 1 in 3

--- Examples/Stereo/stereo_euroc.cc
+++ Examples/Stereo/stereo_euroc.cc
@@ -277,7 +277,7 @@ void LoadImages(const string &strPathLeft, const string &strPathRight, const str
     vstrImageLeft.reserve(5000);
     vstrImageRight.reserve(5000);
     int counter = 0;
-    int skip = 5000;
+    int skip = 3;
     while(!fTimes.eof())
     {
         string s;
